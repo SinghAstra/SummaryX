6b9447fd735f21f6961af5685272263c5f69ac2a
Astra <abhaypratapsinghwd@gmail.com>
2025-05-22 18:56:51 +0530

    Removed unnecassry code

--- app/(home)/hero.tsx
+++ app/(home)/hero.tsx
@@ -13,7 +13,6 @@ import { motion } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import { useEffect } from "react";
 
 interface HeroSectionProps {
   isAuthenticated: boolean;
@@ -30,22 +29,6 @@ const HeroSection = ({ isAuthenticated }: HeroSectionProps) => {
     }
   };
 
-  useEffect(() => {
-    const wakeUpServer = async () => {
-      try {
-        const response = await fetch("/api/wake-up");
-        const data = await response.json();
-        console.log("wakeUpServer Response", data);
-      } catch (error) {
-        if (error instanceof Error) {
-          console.log("error.stack is ", error.stack);
-          console.log("error.message is ", error.message);
-        }
-      }
-    };
-
-    wakeUpServer();
-  }, []);
 
   return (
     <div className="overflow-x-hidden scrollbar-hide ">
